46304077c919dc39bf2e6fdcbd2a6cad334e04dc
Alejandro Lanz <drojanxha@gmail.com>
2021-05-20 20:35:50 +0200

    Actualizamos main

--- src/main/java/com/edesarrollo/alanz/restaurapp/App.java
+++ src/main/java/com/edesarrollo/alanz/restaurapp/App.java
@@ -3,5 +3,6 @@ package com.edesarrollo.alanz.restaurapp;
 public class App {
     public static void main(String[] args) {
         Restaurante principal = new Restaurante();
+        System.out.println("Hola mundo");
     }
 }
